14d158a16b7503b157fdaabbd0f46a0fba1b45ba
kedar.sedai <kedar.sedai05@gmail.com>
2021-12-23 14:51:57 +0545

    Updates

---
+++ precache-manifest.3685f1d616e0566d014618260306cf00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92cc9d58905c73c9b80b0f6178ab5ac",
+    "url": "/Qhub/index.html"
+  },
+  {
+    "revision": "060ee0f2d1bce077d20c",
+    "url": "/Qhub/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f22a5ac4717b2716fc3b",
+    "url": "/Qhub/static/js/2.f5ef3581.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Qhub/static/js/2.f5ef3581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8474d8f97bd84cfb6351",
+    "url": "/Qhub/static/js/3.78319ed7.chunk.js"
+  },
+  {
+    "revision": "060ee0f2d1bce077d20c",
+    "url": "/Qhub/static/js/main.6b3c4918.chunk.js"
+  },
+  {
+    "revision": "486363d031c9f9cc1828",
+    "url": "/Qhub/static/js/runtime-main.b9b91934.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/Qhub/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/Qhub/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/Qhub/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/Qhub/static/media/svg-3.397c455a.svg"
+  }
+]);
